27c96f36755c0cfa8d1bfd33589a3b1b441a1477
Paulo Henrique de Morais <142455846+PauloMorais18@users.noreply.github.com>
2026-07-24 20:45:56 -0300

    b

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-aeb6ecaf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.9g4gqj72h68"
+    "revision": "0.ap1o1f4fho8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
